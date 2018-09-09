53bd99d5ad64bedc6cfc7f3edd575a1e82cbccb4
nhidty99 <43004379+nhidty99@users.noreply.github.com>
2018-09-09 23:13:52 +0700

    Add files via upload

---
+++ Demo/src/BaiTapBuoi1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Yen Nhi
+ */
+public class BaiTapBuoi1
+{
+    public static void main (String[] args)
+    {
+        float a =(float) 1.11;//Đây là ép kiểu
+        float b= (float)1.67;
+        float c= 7;
+      
+        float hieu= a-b-c;
+        System.out.println("In ra man hinh hieu: "+ hieu);
+    }
+    
+}
